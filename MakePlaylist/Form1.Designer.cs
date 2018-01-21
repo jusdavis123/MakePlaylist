32e2fdab0f1df08c8be9be54050498c52b8f6a82
jusdavis123 <35659346+jusdavis123@users.noreply.github.com>
2018-01-21 22:16:36 +0800

    Add files via upload
    Initial  upload

---
+++ MakePlaylist/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace MakePlaylist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.LblItemSelected = new System.Windows.Forms.Label();
+            this.TBFilename = new System.Windows.Forms.TextBox();
+            this.LblItemCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColCheck = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ColNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColLocalPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColPlaylistPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColExtension = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1087, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.importToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(174, 24);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(174, 24);
+            this.importToolStripMenuItem.Text = "Import playlist";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(174, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1087, 555);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Drag and drop files or \r\nplaylist here to begin.\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1087, 555);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 28);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1087, 555);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(928, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(159, 555);
+            this.panel4.TabIndex = 13;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 435);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 89);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Make Playlist";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 101);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 273);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 191);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.LblItemSelected);
+            this.panel3.Controls.Add(this.TBFilename);
+            this.panel3.Controls.Add(this.LblItemCount);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(0, 435);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(888, 119);
+            this.panel3.TabIndex = 9;
+            // 
+            // LblItemSelected
+            // 
+            this.LblItemSelected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblItemSelected.AutoSize = true;
+            this.LblItemSelected.Location = new System.Drawing.Point(771, 11);
+            this.LblItemSelected.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblItemSelected.Name = "LblItemSelected";
+            this.LblItemSelected.Size = new System.Drawing.Size(110, 17);
+            this.LblItemSelected.TabIndex = 4;
+            this.LblItemSelected.Text = "0 Items selected";
+            // 
+            // TBFilename
+            // 
+            this.TBFilename.Location = new System.Drawing.Point(105, 42);
+            this.TBFilename.Margin = new System.Windows.Forms.Padding(4);
+            this.TBFilename.Name = "TBFilename";
+            this.TBFilename.Size = new System.Drawing.Size(289, 22);
+            this.TBFilename.TabIndex = 2;
+            // 
+            // LblItemCount
+            // 
+            this.LblItemCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblItemCount.AutoSize = true;
+            this.LblItemCount.Location = new System.Drawing.Point(599, 11);
+            this.LblItemCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblItemCount.Name = "LblItemCount";
+            this.LblItemCount.Size = new System.Drawing.Size(93, 17);
+            this.LblItemCount.TabIndex = 3;
+            this.LblItemCount.Text = "0 Items found";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(16, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Playlist title";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColCheck,
+            this.ColNumber,
+            this.ColTitle,
+            this.ColLocalPath,
+            this.ColPlaylistPath,
+            this.ColExtension});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(888, 428);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDown);
+            this.dataGridView1.Sorted += new System.EventHandler(this.dataGridView1_Sorted);
+            this.dataGridView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseMove);
+            this.dataGridView1.DragOver += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragOver);
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            // 
+            // ColCheck
+            // 
+            this.ColCheck.HeaderText = "";
+            this.ColCheck.Name = "ColCheck";
+            this.ColCheck.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ColCheck.Width = 30;
+            // 
+            // ColNumber
+            // 
+            this.ColNumber.HeaderText = "#";
+            this.ColNumber.Name = "ColNumber";
+            this.ColNumber.ReadOnly = true;
+            this.ColNumber.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ColNumber.Width = 50;
+            // 
+            // ColTitle
+            // 
+            this.ColTitle.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColTitle.HeaderText = "Title";
+            this.ColTitle.Name = "ColTitle";
+            // 
+            // ColLocalPath
+            // 
+            this.ColLocalPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColLocalPath.HeaderText = "Local Path";
+            this.ColLocalPath.Name = "ColLocalPath";
+            this.ColLocalPath.ReadOnly = true;
+            // 
+            // ColPlaylistPath
+            // 
+            this.ColPlaylistPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColPlaylistPath.HeaderText = "Playlist Path";
+            this.ColPlaylistPath.Name = "ColPlaylistPath";
+            // 
+            // ColExtension
+            // 
+            this.ColExtension.HeaderText = "Type";
+            this.ColExtension.Name = "ColExtension";
+            this.ColExtension.ReadOnly = true;
+            this.ColExtension.Width = 147;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 583);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label LblItemSelected;
+        private System.Windows.Forms.TextBox TBFilename;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label LblItemCount;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColCheck;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColLocalPath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColPlaylistPath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColExtension;
+
+    }
+}
+
---
+++ MakePlaylist/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MakePlaylist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.BringToFront();
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            OnFormDrop((string[])e.Data.GetData(DataFormats.FileDrop));
+            panel2.BringToFront();
+        }
+
+        private void OnFormDrop(string[] files)
+        {
+            if (Path.GetExtension(files[0]) == ".m3u")
+            {
+                TBFilename.Text = Path.GetFileName(@Path.GetFullPath(files[0]));
+                files = File.ReadAllLines(@Path.GetFullPath(files[0]));
+            }
+            
+            PopulateGrid(files);
+        }
+
+        private void PopulateGrid(string[] files)
+        {
+            for (int x = 0; x < files.Length; x++)
+            {
+                //ignore line if it is an extended M3U directive
+                if (files[x][0] == '#')
+                    continue;
+
+                string file = files[x];
+                int rowId = dataGridView1.Rows.Add();
+
+                DataGridViewRow row = dataGridView1.Rows[rowId];
+                row.Cells["ColTitle"].Value = Path.GetFileNameWithoutExtension(file);
+                row.Cells["ColLocalPath"].Value = Path.GetDirectoryName(file);
+                row.Cells["ColPlaylistPath"].Value = "/mnt/internal_sd/Music/Japanese/";
+                row.Cells["ColExtension"].Value = Path.GetExtension(file);
+            }
+
+            UpdateInfo();
+        }
+
+        private void UpdateInfo()
+        {
+            int checkCount = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells["ColNumber"].Value = (row.Index + 1).ToString("N0");
+
+                if (Convert.ToBoolean(row.Cells["ColCheck"].Value))
+                    checkCount++;
+            }
+            dataGridView1.EndEdit();
+
+            LblItemCount.Text = String.Format("{0:n0} Items found", dataGridView1.RowCount);
+            LblItemSelected.Text = String.Format("{0:n0} Items selected", checkCount);
+        }
+
+        private void dataGridView1_Sorted(object sender, EventArgs e)
+        {
+            UpdateInfo();
+            /*
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["ColCheck"].Value))
+                {
+                    dataGridView1.Rows[row.Index].Selected = true;
+                }
+            }
+            */
+
+            //Random ran = new Random();
+            //dataGridView1.Rows[ran.Next(0, dataGridView1.RowCount - 1)].Selected = true;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["ColCheck"].Index && e.RowIndex != -1)
+            {
+                // Handle checkbox state change here
+                if (Convert.ToBoolean(dataGridView1.Rows[e.RowIndex].Cells["ColCheck"].Value))
+                    dataGridView1.Rows[e.RowIndex].Selected = true;
+            }
+        }
+
+
+
+        #region datagrid drop logic
+        private Rectangle dragBoxFromMouseDown;
+        private int rowIndexFromMouseDown;
+        private int rowIndexOfItemUnderMouseToDrop;
+
+        private void dataGridView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                if (dragBoxFromMouseDown != Rectangle.Empty && !dragBoxFromMouseDown.Contains(e.X, e.Y))
+                {
+                    DragDropEffects dropEffect = dataGridView1.DoDragDrop(dataGridView1.Rows[rowIndexFromMouseDown], DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void dataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            rowIndexFromMouseDown = dataGridView1.HitTest(e.X, e.Y).RowIndex;
+
+            if (rowIndexFromMouseDown != -1)
+            {              
+                Size dragSize = SystemInformation.DragSize;
+                dragBoxFromMouseDown = new Rectangle(
+                    new Point(
+                        e.X - (dragSize.Width / 2),
+                        e.Y - (dragSize.Height / 2)
+                    ),
+                    dragSize
+                );
+            }
+            else
+                dragBoxFromMouseDown = Rectangle.Empty;
+        }
+
+        private void dataGridView1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] dropData = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (dropData != null)
+            {
+                OnFormDrop(dropData);
+            }
+            else
+            {
+                Point clientPoint = dataGridView1.PointToClient(new Point(e.X, e.Y));
+                rowIndexOfItemUnderMouseToDrop = dataGridView1.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+
+                if (e.Effect == DragDropEffects.Move)
+                {
+                    DataGridViewRow rowToMove = e.Data.GetData(typeof(DataGridViewRow)) as DataGridViewRow;
+                    dataGridView1.Rows.RemoveAt(rowIndexFromMouseDown);
+
+                    if (rowIndexOfItemUnderMouseToDrop == -1)
+                        dataGridView1.Rows.Insert(dataGridView1.RowCount, rowToMove);
+                    else
+                        dataGridView1.Rows.Insert(rowIndexOfItemUnderMouseToDrop, rowToMove);
+                }
+
+                UpdateInfo();
+            }
+        }
+        #endregion
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (TBFilename.Text == String.Empty)
+            {
+                MessageBox.Show("Enter playlist name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                string path = @TBFilename.Text;
+                if (Path.GetExtension(path) == String.Empty || Path.GetExtension(path) != ".m3u")
+                    path = Path.GetFileNameWithoutExtension(path) + ".m3u";
+
+                try
+                {
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+
+                    //using (StreamWriter file = new StreamWriter(path, Encoding.Unicode))
+                    using (StreamWriter file = new StreamWriter(path, true, Encoding.Unicode))
+                    {
+                        foreach (DataGridViewRow row in dataGridView1.Rows)
+                        {
+                            //string line = row.Cells["ColPlaylistPath"].Value + row.Cells["ColTitle"].Value + row.Cells["ColExtension"].Value;
+                            file.WriteLine(row.Cells["ColPlaylistPath"].Value.ToString() + row.Cells["ColTitle"].Value.ToString() + row.Cells["ColExtension"].Value.ToString());
+                        }
+                    }
+
+                    MessageBox.Show("Playlist created.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }  
+            }
+        }
+    }
+}
