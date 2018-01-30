3b4a0342e06a5085b26ec1577990e2e378246abf
jusdavis123 <35659346+jusdavis123@users.noreply.github.com>
2018-01-30 08:38:06 +0800

    Add files via upload
    Layout update

--- MakePlaylist/About.Designer.cs
+++ MakePlaylist/About.Designer.cs
@@ -46,8 +46,9 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(927, 67);
+            this.label1.Size = new System.Drawing.Size(695, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "MakePlaylist";
             this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -56,9 +57,10 @@
             // 
             this.VLabel.Dock = System.Windows.Forms.DockStyle.Top;
             this.VLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.VLabel.Location = new System.Drawing.Point(0, 67);
+            this.VLabel.Location = new System.Drawing.Point(0, 54);
+            this.VLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.VLabel.Name = "VLabel";
-            this.VLabel.Size = new System.Drawing.Size(927, 23);
+            this.VLabel.Size = new System.Drawing.Size(695, 19);
             this.VLabel.TabIndex = 3;
             this.VLabel.Text = "v1.0.0.0";
             this.VLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -67,9 +69,10 @@
             // 
             this.label4.Dock = System.Windows.Forms.DockStyle.Top;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(0, 90);
+            this.label4.Location = new System.Drawing.Point(0, 73);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(927, 30);
+            this.label4.Size = new System.Drawing.Size(695, 24);
             this.label4.TabIndex = 4;
             this.label4.Text = "Copyright ©  2018 Justin Davis";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -79,11 +82,12 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Control;
             this.flowLayoutPanel1.SetFlowBreak(this.label2, true);
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 10);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
-            this.label2.Size = new System.Drawing.Size(815, 50);
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 8, 0, 16);
+            this.label2.Size = new System.Drawing.Size(585, 39);
             this.label2.TabIndex = 6;
             this.label2.Text = "MakePlaylist is a free and open source playlist creation tool for Android based D" +
                 "igital Audio Players (DAPs).";
@@ -91,11 +95,12 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(23, 60);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 47);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Padding = new System.Windows.Forms.Padding(0, 0, 0, 20);
-            this.label5.Size = new System.Drawing.Size(594, 40);
+            this.label5.Padding = new System.Windows.Forms.Padding(0, 0, 0, 16);
+            this.label5.Size = new System.Drawing.Size(428, 31);
             this.label5.TabIndex = 7;
             this.label5.Text = "For issues, comments or additional feature requests, feel free to contact me at";
             // 
@@ -103,11 +108,12 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.flowLayoutPanel1.SetFlowBreak(this.linkLabel1, true);
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(623, 60);
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(449, 47);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 20);
-            this.linkLabel1.Size = new System.Drawing.Size(190, 40);
+            this.linkLabel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 16);
+            this.linkLabel1.Size = new System.Drawing.Size(142, 31);
             this.linkLabel1.TabIndex = 8;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "jusdavis123@gmail.com";
@@ -121,19 +127,21 @@
             this.flowLayoutPanel1.Controls.Add(this.label6);
             this.flowLayoutPanel1.Controls.Add(this.linkLabel2);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 120);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 97);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(20, 10, 20, 0);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(927, 150);
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(15, 8, 15, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(695, 122);
             this.flowLayoutPanel1.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(23, 100);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 78);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(418, 20);
+            this.label6.Size = new System.Drawing.Size(303, 15);
             this.label6.TabIndex = 10;
             this.label6.Text = "If you would like to donate to the developer, please visit";
             // 
@@ -141,10 +149,11 @@
             // 
             this.linkLabel2.AutoSize = true;
             this.flowLayoutPanel1.SetFlowBreak(this.linkLabel2, true);
-            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel2.Location = new System.Drawing.Point(447, 100);
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(324, 78);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(177, 20);
+            this.linkLabel2.Size = new System.Drawing.Size(132, 15);
             this.linkLabel2.TabIndex = 11;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "paypal.me/jusdavis123";
@@ -154,23 +163,25 @@
             // 
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label3.Location = new System.Drawing.Point(0, 120);
+            this.label3.Location = new System.Drawing.Point(0, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.label3.Size = new System.Drawing.Size(927, 2);
+            this.label3.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Size = new System.Drawing.Size(695, 2);
             this.label3.TabIndex = 10;
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(927, 270);
+            this.ClientSize = new System.Drawing.Size(695, 219);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.VLabel);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "About";
             this.Text = "About";
             this.flowLayoutPanel1.ResumeLayout(false);
